dc320c3a8897c38705f14e0492031a83dd934d2a
R <rem0506@hotmail.com>
2020-07-18 16:56:05 +0530

    Update circle_of_squares.py
    added comments

--- circle_of_squares.py
+++ circle_of_squares.py
@@ -1,18 +1,18 @@
+#import the turtle library
 import turtle
 
+#assign
 my_turtle=turtle.Turtle()
 
-import turtle
-
-
-my_turtle = turtle.Turtle()
-
+# define a squar function which draws a square, and
+# also shifts the angle after one square is drawn
 def squar(len,ang,shift_angle):
   for i in range(0,4):
     my_turtle.forward(len)
     my_turtle.right(ang)
   my_turtle.right(shift_angle)
   
+#call the square function
 for i in range(0,36):
   squar(100,90,100)
   
